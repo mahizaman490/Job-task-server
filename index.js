b89ded9139ed5d37b82ea65c731e97ff91fc7d8b
Mahi Zaman <mahizaman490@gmail.com>
2023-12-22 14:36:30 +0600

    loaded data from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,8 +37,9 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://mahizaman490:vJVTqd3OX5Ffq331@cluster0.sjpq5n9.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjpq5n9.mongodb.net/your-database?retryWrites=true&w=majority`;
 
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -51,6 +53,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const taskCollection = client.db('taskDB').collection('task')
+
+
+app.get('/task',async(req,res)=>{
+  const cursor = taskCollection.find();
+  const result = await cursor.toArray();
+  res.send(result)
+})
+
+
+
+
+
+
+
+
+    app.post('/task',async(req,res)=>{
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await taskCollection.insertOne(newProduct);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
